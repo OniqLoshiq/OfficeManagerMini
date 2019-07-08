23825cc4fb95edcbb9bd51412d4fb25f85847ae3
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-08 11:15:31 +0300

    Setup Management Area routing and empty skeleton with different sidebar

---
+++ OfficeManagerMini/OMM.App/Areas/Management/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMM.App.Areas.Management.Controllers
+{
+    [Area("Management")]
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
