ea12975a74c521b2af30e040f58f47dfc06e2917
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-10 17:45:52 +0300

    Added tinymce text editor plugin

---
+++ OfficeManagerMini/OMM.App/Controllers/AssignmentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMM.App.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ OfficeManagerMini/OMM.App/wwwroot/js/run-tinymce.js
@@ -0,0 +1,14 @@
+﻿tinymce.init({
+    selector: '#mytextarea',
+    /* plugin */
+    plugins: [
+        "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        "save table contextmenu directionality emoticons template paste textcolor"
+    ],
+
+    /* toolbar */
+    toolbar: "insertfile undo redo | styleselect fontselect fontsizeselect | bold italic underline| alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullscreen | forecolor backcolor hr emoticons",
+})
+
+//https://www.tiny.cloud/docs/advanced/editor-control-identifiers/#toolbarcontrols
