8e4956ad5a287a9a06723c43ca54b7b8d7fd8d30
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-12 23:26:54 +0300

    Finished create assignment view

--- OfficeManagerMini/OMM.App/wwwroot/js/site.js
+++ OfficeManagerMini/OMM.App/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function myProjects() {
+    var checkBox = document.getElementById("isProjectRelated");
+    var projectsList = document.getElementById("toggle-checkbox");
+    if (checkBox.checked == false) {
+        $("#project").prop("disabled", true);
+        $(".selectpicker[data-id='project']").addClass("disabled");
+        $('select[name=project]').val(0);
+        $('.selectpicker').selectpicker('refresh')
+        projectsList.disabled = true;
+    } else {
+        projectsList.disabled = false;
+        $("#project").prop("disabled", false);
+        $(".selectpicker[data-id='project']").removeClass("disabled");
+        $('.selectpicker').selectpicker('refresh')
+
+    }
+}
+
