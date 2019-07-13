8448dfd713554678937d3f7107d3c46cb82e89c9
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-13 22:00:54 +0300

    Added Management Create Project view

---
+++ OfficeManagerMini/OMM.App/Areas/Management/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMM.App.Areas.Management.Controllers
+{
+    [Area("Management")]
+    public class ProjectsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
