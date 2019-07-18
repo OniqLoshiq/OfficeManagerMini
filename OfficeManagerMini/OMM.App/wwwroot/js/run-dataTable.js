cebdcb13a8dc6d7c65db0c6c54508f1b23693216
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-18 17:28:36 +0300

    Add edit and release employee views

--- OfficeManagerMini/OMM.App/wwwroot/js/run-dataTable.js
+++ OfficeManagerMini/OMM.App/wwwroot/js/run-dataTable.js
@@ -1,21 +1,23 @@
 ﻿$(document).ready(function () {
-    var table = $('#myDataTable').DataTable({
-        buttons: ['copy', 'excel', 'pdf',
-            {
-                extend: 'print',
-                exportOptions: {
-                    stripHtml: false,
-                }
-            },
+    var id0 = document.getElementById("myDataTable");
+    if (id0) {
+        var table = $('#myDataTable').DataTable({
+            buttons: ['copy', 'excel', 'pdf',
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        stripHtml: false,
+                    }
+                },
 
-            'colvis']
-    });
+                'colvis']
+        });
+        table.buttons().container()
+            .appendTo('#myDataTable_wrapper .col-md-6:eq(0)');
+    }
 
-    table.buttons().container()
-        .appendTo('#myDataTable_wrapper .col-md-6:eq(0)');
-
-    var id = document.getElementById("myDataTableInactiveEmpl");
-    if (id) {
+    var id1 = document.getElementById("myDataTableInactiveEmpl");
+    if (id1) {
         var table2 = $('#myDataTableInactiveEmpl').DataTable({
             buttons: ['copy', 'excel', 'pdf',
                 {
@@ -31,4 +33,24 @@
         table2.buttons().container()
             .appendTo('#myDataTableInactiveEmpl_wrapper .col-md-6:eq(0)');
     }
+
+    var id2 = document.getElementById("myDataTableReleasedItems");
+    if (id2) {
+        var table3 = $('#myDataTableReleasedItems').DataTable({
+            buttons: ['pdf',
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        stripHtml: false,
+                    }
+                },
+
+                'colvis']
+        });
+
+        table3.buttons().container()
+            .appendTo('#myDataTableReleasedItems_wrapper .col-md-6:eq(0)');
+    }
+
+
 });
