df8523c83718f3f6a4c2635f1ebdfe4f8b6fa128
OniqLoshiq <doncho.vasilev@gmail.com>
2019-07-20 23:41:19 +0300

    Move the libraries and mvc app to new structured folders in the solution

---
+++ OfficeManagerMini/03_App/OMM.App/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace OMM.App.Common
+{
+    public static class Constants
+    {
+        public const string POLICY_PREFIX = "MinimumAccessLevel";
+
+        public const string ACCESS_LEVEL_CLAIM = "AccessLevel";
+    }
+}
