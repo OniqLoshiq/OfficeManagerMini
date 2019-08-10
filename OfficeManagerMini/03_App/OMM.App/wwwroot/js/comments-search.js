2f9d826dfa39be876f60f57768f2eef1fabdfadf
OniqLoshiq <doncho.vasilev@gmail.com>
2019-08-11 00:34:25 +0300

    Add assignment view with comments and custom search

---
+++ OfficeManagerMini/03_App/OMM.App/wwwroot/js/comments-search.js
@@ -0,0 +1,14 @@
+﻿$('#comments-search-bar').on("change paste keyup", function () {
+    $('.comment').show();
+
+    let searchValue = $('#comments-search-bar').val();
+
+    for (let elem of $('.comment')) {
+        let commentHeader = $($(elem).children()[0]).text();
+        let commentDescription = $($(elem).children()[1]).text();
+
+        if (!commentHeader.toLowerCase().includes(searchValue.toLowerCase()) && !commentDescription.toLowerCase().includes(searchValue.toLowerCase())) {
+            $(elem).hide();
+        }
+    }
+});
