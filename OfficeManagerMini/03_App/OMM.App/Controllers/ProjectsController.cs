313287594863de03c69a4cc44a3bca3c3094eae6
OniqLoshiq <doncho.vasilev@gmail.com>
2019-08-15 17:21:47 +0300

    Create project create files

---
+++ OfficeManagerMini/03_App/OMM.App/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMM.App.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
