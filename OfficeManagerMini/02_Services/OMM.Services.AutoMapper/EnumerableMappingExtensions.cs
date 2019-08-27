5cf2c82f5fb12156dcf9379269c4680097dcad96
OniqLoshiq <doncho.vasilev@gmail.com>
2019-08-27 20:08:59 +0300

    Add automapper IEnumerable map

---
+++ OfficeManagerMini/02_Services/OMM.Services.AutoMapper/EnumerableMappingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace OMM.Services.AutoMapper
+{
+    public static class EnumerableMappingExtensions
+    {
+        public static IEnumerable<TDestination> To<TDestination>(
+           this IEnumerable source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var src in source)
+            {
+                yield return Mapper.Map<TDestination>(src);
+            }
+        }
+    }
+}
